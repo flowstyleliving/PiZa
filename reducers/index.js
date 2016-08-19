5bbf5d082a8e44725262a6d230c22816efcc3e19
flowstyleliving <mys628@gmail.com>
2016-08-18 17:04:33 -0700

    staging

--- reducers/index.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from 'redux'
-// import { reduxReactFirebase, firebaseStateReducer } from 'redux-react-firebase'
-import recipeReducer from './recipeReducer'
-
-const rootReducer = combineReducers({
-  recipes: recipeReducer
-})
-
-export default rootReducer
